01246b792987bcf37fee04b60a90b65163dedd1f
emerrafter1 <emerlouiserafter@gmail.com>
2025-05-05 10:59:03 +0100

    add commands

--- main.py
+++ main.py
@@ -2,15 +2,33 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
-screen.listen()
+tim.pensize(1)
+tim.speed("fastest")
+
 
 def move_forwards():
-    tim.fd(20)
+    tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
 
+def turn_left():
+    tim.left(10)
 
+def turn_right():
+    tim.right(10)
 
-screen.onkey(key = "space", fun=move_forwards)
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
+screen.listen()
+screen.onkey(move_forwards, "Up")
+screen.onkey(move_backwards, "Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+screen.onkey(clear, "c")
 
 screen.exitonclick()
